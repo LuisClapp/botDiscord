708b708a82695545bfc2680ac866798747a13160
Luís Clapp <fhelipebarros@hotmail.com>
2025-10-31 10:50:11 -0300

    Revert "update"
    This reverts commit a21d4e386f72720e134e98dfe513244ed892ce86.

--- .husky/husky.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-if [ -z "$husky_skip_init" ]; then
-  debug () {
-    [ "$HUSKY_DEBUG" = "1" ] && echo "husky (debug) - $1"
-  }
-
-  readonly hook_name="$(basename "$0")"
-  debug "starting $hook_name hook"
-fi
